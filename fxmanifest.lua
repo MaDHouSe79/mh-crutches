dfe1126d68176505802e251c2f38cf1092a3e6c7
MaDHouSe79 <madhouse1979@gmail.com>
2022-10-02 16:47:49 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,17 +6,17 @@ description 'A useable item script for crutches!'
 version '1.0.0'
 
 shared_scripts {
-	'@qb-core/shared/locale.lua',
-	'locales/nl.lua', -- change en to your language
-	'config.lua',
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua', -- change en to your language
+    'config.lua',
 }
 
 client_scripts {	
-	"client/main.lua"
+    "client/main.lua"
 }
 
 server_scripts {	
-	"server/main.lua"
+    "server/main.lua"
 }
 
 dependencies {
